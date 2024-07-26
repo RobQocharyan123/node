2a0d7de7ac290f7f6889069577480382661dc96d
RobQocharyan <robqocharyan@mail.ru>
2024-07-26 21:03:05 +0400

    g

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
