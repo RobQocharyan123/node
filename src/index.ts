9f704b8449e26ac9036f232765c46bd865bcac12
RobQocharyan <robqocharyan@mail.ru>
2024-07-26 21:57:39 +0400

    h

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('<h1>Barev Nare<h1>');
 });
 
 app.listen(port, () => {
